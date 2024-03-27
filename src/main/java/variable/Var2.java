834c9a3c755cb1b4bdaf15106839d4ee2fc4ac1b
hyung8 <choseo7722@gmail.com>
2024-03-27 23:40:58 +0900

    variable 1

---
+++ src/main/java/variable/Var2.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var2 {
+
+	public static void main(String[] args) {
+		
+		int a; //변수 선수   ->그릇을 만든 것.
+		a = 10; // 변수 초기화    -> 그릇에 값을 담았다.
+		
+		System.out.println(a);
+		a = 20;
+		System.out.println(a);
+		a = 30;
+		System.out.println(a);
+	}
+
+}
