e17c8817754e0745ff45f75ab4c1365e43ddbc19
hyung8 <choseo7722@gmail.com>
2024-03-27 23:03:35 +0900

    java start

---
+++ src/main/java/javaStart/HelloJava.java
@@ -0,0 +1,12 @@
+package javaStart;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello Java");
+		// TODO Auto-generated method stub
+
+	}
+
+}
