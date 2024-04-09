e322a42aebf02128f4b552c51b1487aaa99b6b98
hyung8 <choseo7722@gmail.com>
2024-04-10 06:48:47 +0900

    operator3

--- src/main/java/operator/operator3.java
+++ src/main/java/operator/operator3.java
@@ -4,6 +4,9 @@ public class operator3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		//문자열 비교는 rquals
 
 		
 		String str1 = "문자열1";
@@ -21,6 +24,9 @@ public class operator3 {
 		System.out.println(result1);
 		System.out.println(result2);
 		System.out.println(result3);
+		
+		
+		System.out.println("hello" == "hello"); //특정한 조건에서 true가 나오지 않는 경우가 있으므로 문자열 비교는 "==" 를 라용하지 않는다.
 	}
 
 }
