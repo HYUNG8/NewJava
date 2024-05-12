80b84b0b376a29ba1f8edf6e3a1e2ffdb571f4fb
hyung8 <choseo7722@gmail.com>
2024-05-12 16:39:51 +0900

    CondOp

--- src/main/java/condition/CondOp.java
+++ src/main/java/condition/CondOp.java
@@ -17,9 +17,9 @@ public class CondOp {
 		}
 		System.out.println("age = "+ age + " status = " + grade);
 		
-		//CondOp
+		// ondOp
 		// 삼항연산자는 항이 세개이다. 조건 ? 참_표현식 : 거짓_표현식
-		//단순히 참과 거짓을 이용해서 특정 값을 구하는 경우는 삼항연산자가 간결하게 코드를 작성할 수 있다.
+		// 단순히 참과 거짓을 이용해서 특정 값을 구하는 경우는 삼항연산자가 간결하게 코드를 작성할 수 있다.
 		
 		age = 18;
 		String status = (age > 18) ? "성인" : "미성년자";
