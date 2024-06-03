8eb7977a9ba6f83c4361034a9d3596153cc224d2
hyung8 <choseo7722@gmail.com>
2024-06-03 23:10:57 +0900

    Method

---
+++ src/main/java/method/Method1Ref.java
@@ -0,0 +1,31 @@
+package method;
+
+public class Method1Ref {
+
+	public static void main(String[] args) {
+		
+		
+		// 계산1 
+		
+		int a = 1;
+		int b = 2;
+		
+		System.out.println(a + "+" + b + " 연산 수행");
+		
+		int result = a + b;
+		
+		System.out.println("결과값: " + result);
+		
+		// 계산2
+		
+		int x = 10;
+		int y = 20;
+		
+		System.out.println(x + "+" + y + " 연산 수행");
+		
+		int result2 = x + y;
+		
+		System.out.println("연산결과: " + result2);
+	}
+
+}
