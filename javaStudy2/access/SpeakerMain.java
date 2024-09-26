3f6a00f1547af4761ad69907bf85ff96b4f578ac
hyung8 <choseo7722@gmail.com>
2024-09-26 22:19:25 +0900

    access

--- javaStudy2/access/SpeakerMain.java
+++ javaStudy2/access/SpeakerMain.java
@@ -19,11 +19,14 @@ public class SpeakerMain {
 		speaker.volumeUp();
 		
 		
-		// 필드에 접근
-		System.out.println("volume 필드 접근 수정");
+		// 필드에 직접 접근
+		System.out.println("volume 필드 직접 접근 수정");
 		speaker.volume = 200;
 		speaker.showVolume();
 		
+		// 메서드에서 필드의 값에 100이라는 제한을 두었지만 
+		// 필드에 직접 접근해서 값을 변경하게 되면 제한을 무시하고 원하는 값을 설정할 수 있게 된다.
+		// 이러한 문제를 근본적으로 해결하기 위해서는 필드의 외부 접근을 막을 수 있는 방법이 필요하다.
 		
 
 	}
