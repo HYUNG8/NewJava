753ad0a0e7a18eacd06c76c9dc79556ae8040b09
hyung8 <choseo7722@gmail.com>
2024-10-14 23:22:40 +0900

    static

--- javaStudy2/static1/DecoDataMain2.java
+++ javaStudy2/static1/DecoDataMain2.java
@@ -1,19 +1,22 @@
 package static1;
 
-public class DecoDataMain {
+
+import static static1.DecoData.*; // 정적 변수와 정적 메서드를 부를때 클래스 명을 생략 가능하다.
+
+public class DecoDataMain2 {
 
 	public static void main(String[] args) {
 		
 		
 		System.out.println("1. 정적 호출");
-		DecoData.staticCall();
+		staticCall();
 		
 		System.out.println("2. 인스턴스 호출");
 		DecoData data = new DecoData();
 		data.instanceCall();
 		
 		System.out.println("3. 참조값 지정");
-		DecoData.staticCall2(data);
+		staticCall2(data);
 		
 		
 		
@@ -23,7 +26,7 @@ public class DecoDataMain {
 		data3.staticCall();		// 에러가 발생하지는 않지만, 클래스메서드가 아닌 인스턴스메서드 처럼 보이기 때문에 사용하지 않는다.
 		
 		// 클래스를 통한 접근
-		DecoData.staticCall();
+		staticCall();
 		
 	}
 
